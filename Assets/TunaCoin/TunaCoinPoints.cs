4f0d434f0f1267fffdfa14352cfe56cbe0b41e06
Jacob Roberts <jjake1232@gmail.com>
2021-08-07 03:38:33 -0400

    SCipts
    :(

--- Assets/TunaCoin/TunaCoinPoints.cs
+++ Assets/TunaCoin/TunaCoinPoints.cs
@@ -9,6 +9,8 @@ public class TunaCoinPoints : MonoBehaviour
     // right now, button press to add points, make points change the score (D-S) 
     // screen to tally points and give score, option to restart, or back to main menu
     // also think about option for the escape menu to restart level or go back to main menu.
+
+    // close the game loop so we can go from main menu to play, back to main menu.
     void Start()
     {
         
