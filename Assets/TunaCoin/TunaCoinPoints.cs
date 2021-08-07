a07344f32c7bbab81d243151b899181cdcfee41c
Jacob Roberts <jjake1232@gmail.com>
2021-08-07 03:28:42 -0400

    TunaCoin UI

---
+++ Assets/TunaCoin/TunaCoinPoints.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunaCoinPoints : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // right now, button press to add points, make points change the score (D-S) 
+    // screen to tally points and give score, option to restart, or back to main menu
+    // also think about option for the escape menu to restart level or go back to main menu.
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
